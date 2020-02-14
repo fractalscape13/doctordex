89a997146c2722e79a2c901d2d9727f83a00b7ec
Joseph Wangemann <fractalscape13@gmail.com>
2020-02-14 09:46:38 -0800

    update doctor-service.js with api call urls

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,7 +1,7 @@
 export class DoctorService {
   async getDoctorByName(name) {
     try {
-      let response = await fetch('api url');
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${API_KEY}`);
       let jsonifiedResponse;
       if (response.status == 200 && response.ok) {
         jsonifiedResponse = await response.json();
@@ -9,14 +9,14 @@ export class DoctorService {
         jsonifiedResponse = false;
       }
       return jsonifiedResponse;
-    } catch {
+    } catch(error) {
       return false;
     }
   }
 
-  async getDoctorByIssue(name) {
+  async getDoctorByIssue(issue) {
     try {
-      let response = await fetch('api url');
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${API_KEY}`);
       let jsonifiedResponse;
       if (response.status == 200 && response.ok) {
         jsonifiedResponse = await response.json();
@@ -24,9 +24,8 @@ export class DoctorService {
         jsonifiedResponse = false;
       }
       return jsonifiedResponse;
-    } catch {
+    } catch(error) {
       return false;
     }
   }
-
 }
