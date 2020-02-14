4e99be31664f2edd663fad3bcfa86eee29260e3e
Joseph Wangemann <fractalscape13@gmail.com>
2020-02-14 10:04:45 -0800

    add if/else to check for empty input, show emptyinput message on click

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,7 +1,7 @@
 export class DoctorService {
   async getDoctorByName(name) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse;
       if (response.status == 200 && response.ok) {
         jsonifiedResponse = await response.json();
@@ -14,9 +14,9 @@ export class DoctorService {
     }
   }
 
-  async getDoctorByIssue(issue) {
+  async getDoctorByCondition(condition) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse;
       if (response.status == 200 && response.ok) {
         jsonifiedResponse = await response.json();
--- src/main.js
+++ src/main.js
@@ -2,11 +2,29 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import { DoctorService } from './doctor-service';
+import { DoctorService } from './doctor-service';
 
 $(document).ready(function() {
   $("form").submit(function() {
     event.preventDefault();
-    
+    let name = $("input#name").val();
+    let condition = $("input#condition").val();
+    (async () => {
+      let newQuery = new DoctorService();
+      let response;
+      if (name) {
+        response = await newQuery.getDoctorByName(name);
+      } else if (condition) {
+        response = await newQuery.getDoctorByCondition(condition);
+      } else {
+        $("#emptyinput").fadeIn();
+        setTimeout(function() {
+          $("#emptyinput").fadeOut();
+        }, 3000);
+      }
+      if (response.length == 0) {
+        $("#output").text("Your query returned no results, please try again");
+      }
+    })();
   });
 });
